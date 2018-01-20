7560fc96446bc73a52373d61d8fa2944bcb8b852
Kermit Vestal <kermitv@gmail.com>
2018-01-20 11:19:00 -0700

    implemented relops for I32_eq, I32_eqz, and I32_ne

---
+++ src/main/java/happynewmoonwithreport/opcode/I32_eq.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2017 Whole Bean Software, LTD.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package happynewmoonwithreport.opcode;
+
+
+import java.util.UUID;
+
+import happynewmoonwithreport.WasmInstanceInterface;
+import happynewmoonwithreport.WasmRuntimeException;
+import happynewmoonwithreport.WasmStack;
+import happynewmoonwithreport.type.I32;
+import happynewmoonwithreport.type.UInt32;
+
+/**
+ * I32 equal to (i32_eq)
+ * <p>
+ * <b>Note this is the same for all Relative Operations</b>
+ * <p>
+ * t.relop
+ * <ol>
+ * <li>
+ * Assert: due to validation, two values of value type t are on the top of the stack.
+ * </li>
+ * <li>
+ * Pop the value t.const c2 from the stack.
+ * </li>
+ * <li>
+ * Pop the value t.const c1 from the stack.
+ * </li>
+ * <li>
+ * Let c be the result of computing relopt(c1,c2).
+ * </li>
+ * <li>
+ * Push the value i32.const c to the stack.
+ * <p>
+ * </li>
+ * </ol>
+ * <p>
+ * <p>
+ * Source:  <a href="https://webassembly.github.io/spec/exec/instructions.html#exec-relop" target="_top">
+ * https://webassembly.github.io/spec/exec/instructions.html#exec-relop
+ * </a>
+ */
+public class I32_eq {
+    private WasmInstanceInterface instance;
+
+    private I32_eq() {
+        super();
+    }
+
+    public I32_eq(WasmInstanceInterface instance) {
+        this();
+        this.instance = instance;
+    }
+
+
+    /**
+     * Execute the opcode.
+     */
+    public void execute() {
+        WasmStack<Object> stack = instance.stack();
+        if ((stack.peek() instanceof I32) == false) {
+            throw new WasmRuntimeException(UUID.fromString("03da0147-6ed0-4039-8f54-a7cab2477b6f"),
+                    "i32_eq: Value2 type is incorrect");
+        }
+        I32 value2 = (I32) stack.pop();
+
+        if ((stack.peek() instanceof I32) == false) {
+            throw new WasmRuntimeException(UUID.fromString("3d0a4b3c-bf4b-42b0-b594-c57a4cb23143"),
+                    "i32_eq: Value1 type is incorrect");
+        }
+        I32 value1 = (I32) stack.pop();
+
+        Integer iResult;
+        if (value1.equals(value2)) {
+            iResult = 1;
+        } else {
+            iResult = 0;
+        }
+        I32 result = new I32(iResult);
+
+        stack.push(result);
+    }
+
+
+}
---
+++ src/main/java/happynewmoonwithreport/opcode/I32_ne.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2017 Whole Bean Software, LTD.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package happynewmoonwithreport.opcode;
+
+
+import java.util.UUID;
+
+import happynewmoonwithreport.WasmInstanceInterface;
+import happynewmoonwithreport.WasmRuntimeException;
+import happynewmoonwithreport.WasmStack;
+import happynewmoonwithreport.type.I32;
+import happynewmoonwithreport.type.UInt32;
+
+/**
+ * I32 equal to (i32_eq)
+ * <p>
+ * <b>Note this is the same for all Relative Operations</b>
+ * <p>
+ * t.relop
+ * <ol>
+ * <li>
+ * Assert: due to validation, two values of value type t are on the top of the stack.
+ * </li>
+ * <li>
+ * Pop the value t.const c2 from the stack.
+ * </li>
+ * <li>
+ * Pop the value t.const c1 from the stack.
+ * </li>
+ * <li>
+ * Let c be the result of computing relopt(c1,c2).
+ * </li>
+ * <li>
+ * Push the value i32.const c to the stack.
+ * <p>
+ * </li>
+ * </ol>
+ * <p>
+ * <p>
+ * Source:  <a href="https://webassembly.github.io/spec/exec/instructions.html#exec-relop" target="_top">
+ * https://webassembly.github.io/spec/exec/instructions.html#exec-relop
+ * </a>
+ */
+public class I32_ne {
+    private WasmInstanceInterface instance;
+
+    private I32_ne() {
+        super();
+    }
+
+    public I32_ne(WasmInstanceInterface instance) {
+        this();
+        this.instance = instance;
+    }
+
+
+    /**
+     * Execute the opcode.
+     */
+    public void execute() {
+        WasmStack<Object> stack = instance.stack();
+        if ((stack.peek() instanceof I32) == false) {
+            throw new WasmRuntimeException(UUID.fromString("c0567b29-8821-4db4-82f0-58780682c917"),
+                    "I32_ne: Value2 type is incorrect");
+        }
+        I32 value2 = (I32) stack.pop();
+
+        if ((stack.peek() instanceof I32) == false) {
+            throw new WasmRuntimeException(UUID.fromString("5878a528-f6b1-48c4-a6e5-0c6e955874cb"),
+                    "I32_ne: Value1 type is incorrect");
+        }
+        I32 value1 = (I32) stack.pop();
+
+        Integer iResult;
+        if (value1.equals(value2) == false) {
+            iResult = 1;
+        } else {
+            iResult = 0;
+        }
+        I32 result = new I32(iResult);
+
+        stack.push(result);
+    }
+
+
+}
