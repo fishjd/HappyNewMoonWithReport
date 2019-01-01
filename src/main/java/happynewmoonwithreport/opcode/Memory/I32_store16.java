91524bff041eb8e27acddd8d4c2e00daa1d5bcfd
James Haring <fishjd@gmail.com>
2019-01-01 15:41:52 -0700

    Refactor I32 Store opcodes
    Move most of the code to StoreBase.

--- src/main/java/happynewmoonwithreport/opcode/Memory/I32_store16.java
+++ src/main/java/happynewmoonwithreport/opcode/Memory/I32_store16.java
@@ -17,50 +17,47 @@
 package happynewmoonwithreport.opcode.Memory;
 
 
-import java.util.UUID;
-
 import happynewmoonwithreport.WasmFrame;
-import happynewmoonwithreport.WasmRuntimeException;
 import happynewmoonwithreport.WasmStack;
 import happynewmoonwithreport.WasmStore;
-import happynewmoonwithreport.type.I32;
 import happynewmoonwithreport.type.JavaType.ByteUnsigned;
 import happynewmoonwithreport.type.MemoryArgument;
 import happynewmoonwithreport.type.MemoryType;
 import happynewmoonwithreport.type.U32;
-import happynewmoonwithreport.type.UInt32;
 
 /**
- * <h1>i32_store16</h1> Load 16 bits from the stack to memory.
- * <p>
+ * <h1>i32_store16</h1> Store 16 bits from the stack to memory.
  * <p>
  * Memory Instructions<br>
  * <p>
- * <b>Source:</b>  <a href="https://webassembly.github.io/spec/core/syntax/instructions.html#memory-instructions" target="_top">
- * https://webassembly.github.io/spec/core/syntax/instructions.html#memory-instructions
+ * <b>Source:</b>
+ * <a href="https://webassembly.github.io/spec/core/syntax/instructions.html#memory-instructions"
+ * target="_top"> https://webassembly.github.io/spec/core/syntax/instructions
+ * .html#memory-instructions
  * </a>
  * <br>
  * <p>
  * Memory Overview<br>
- * <b>Source:</b>  <a href="https://webassembly.github.io/spec/core/syntax/instructions.html#syntax-instr-memory" target="_top">
- * https://webassembly.github.io/spec/core/syntax/instructions.html#syntax-instr-memory
+ * <b>Source:</b>
+ * <a href="https://webassembly.github.io/spec/core/syntax/instructions.html#syntax-instr-memory"
+ * target="_top"> https://webassembly.github.io/spec/core/syntax/instructions
+ * .html#syntax-instr-memory
  * </a>
  * </p>
  * <br>
  * Exec:
  * <p>
- * <b>Source:</b>  <a href="https://webassembly.github.io/spec/core/exec/instructions.html#exec-storen" target="_top">
- * https://webassembly.github.io/spec/core/exec/instructions.html#exec-storen
+ * <b>Source:</b>
+ * <a href="https://webassembly.github.io/spec/core/exec/instructions.html#exec-storen"
+ * target="_top"> https://webassembly.github.io/spec/core/exec/instructions.html#exec-storen
  * </a>
- * <p>
  * <h2>t.store memarg and t.storeN memarg</h2>
  * <ol>
  * <li>
  * Let F  be the current frame.
  * </li>
  * <li>
- * Assert: due to validation, F.module.memaddrs[0]
- * exists.
+ * Assert: due to validation, F.module.memaddrs[0] exists.
  * </li>
  * <li>Let a be the memory address F.module.memaddrs[0]
  * </li>
@@ -68,32 +65,25 @@ import happynewmoonwithreport.type.UInt32;
  * Assert: due to validation, S.mems[a] exists.
  * </li>
  * <li>
- * Let mem
- * be the memory instance S.mems[a]..
+ * Let mem be the memory instance S.mems[a]..
  * </li>
  * <li>
- * Assert: due to validation, a value of value type t
- * is on the top of the stack.
+ * Assert: due to validation, a value of value type t is on the top of the stack.
  * </li>
  * <li>
- * Pop the value t.const c
- * from the stack.
+ * Pop the value t.const c from the stack.
  * </li>
  * <li>
- * Assert: due to validation, a value of value type i32
- * is on the top of the stack.
+ * Assert: due to validation, a value of value type i32 is on the top of the stack.
  * </li>
  * <li>
- * Pop the value i32.const i
- * from the stack.
+ * Pop the value i32.const i from the stack.
  * </li>
  * <li>
- * Let ea
- * be the integer i+memarg.offset
+ * Let ea be the integer i+memarg.offset
  * </li>
  * <li>
- * If N
- * is not part of the instruction, then:
+ * If N is not part of the instruction, then:
  * <ul>
  * <li>
  * Let N be the bit width |t| of value type t
@@ -101,9 +91,7 @@ import happynewmoonwithreport.type.UInt32;
  * </ul>
  * </li>
  * <li>
- * If ea+N/8
- * is larger than the length of mem.data
- * , then:
+ * If ea+N/8 is larger than the length of mem.data , then:
  * <ul>
  * <li>
  * Trap.
@@ -111,18 +99,15 @@ import happynewmoonwithreport.type.UInt32;
  * </ul>
  * </li>
  * <li>
- * If N
- * is part of the instruction, then:
+ * If N is part of the instruction, then:
  * <ul>
  * <li>
- * Let n
- * <p>
- * be the result of computing wrap|t|,N(c)
+ * Let n be the result of computing wrap|t|,N(c)
  * </li>
- * <li>.
- * Let b*
- * be the byte sequence bytesiN(n)
+ * <li>
+ * Let b* be the byte sequence bytesiN(n)
  * </li>
+ * </ul>
  * </li>
  * <li>
  * Else:
@@ -134,139 +119,26 @@ import happynewmoonwithreport.type.UInt32;
  * </li>
  * <li>
  * <p>
- * Replace the bytes mem.data[ea:N/8]
- * with b*.
+ * Replace the bytes mem.data[ea:N/8] with b*.
  * </li>
  * </ol>
  */
-public class I32_store16 extends LoadBase {
-
-	private MemoryArgument memoryArgument;
-	private WasmFrame frame;
-	private WasmStore store;
-	private WasmStack stack;
+public class I32_store16 extends StoreBase {
 
 	private I32_store16() {
 		super();
 	}
 
-	public I32_store16(MemoryArgument memoryArgument, WasmFrame frame, WasmStore store, WasmStack stack) {
-		this();
-		this.memoryArgument = memoryArgument;
-		this.frame = frame;
-		this.store = store;
-		this.stack = stack;
+	public I32_store16(MemoryArgument memoryArgument, WasmFrame frame, WasmStore store,
+		WasmStack stack) {
+		super(memoryArgument, frame, store, stack);
+
+		N = new U32(16);
 	}
 
-
-	/**
-	 * Execute the opcode.
-	 */
-	public void execute() {
-		U32 N = new U32(16);
-		U32 t = new U32(32);
-		Object T = new I32(0);
-
-		// 1. Let F be the current frame.
-		// Frame set in constructor.
-
-		// 2. Assert: due to validation, F.module.memaddrs[0] exists.
-		UInt32 memoryIndex = new UInt32(0);
-		final Boolean memoryExists = frame.getModule().memoryExists(memoryIndex);
-		if (memoryExists == false) {
-			throw new WasmRuntimeException(UUID.fromString("fca5ea7c-cb2d-4d23-9d07-c5c98291e984"),
-					"Memory %s does not exists", memoryIndex);
-		}
-
-		// 3. Let a be the memory address F.module.memaddrs[0].
-		final MemoryType a = frame.getModule().getMemory(memoryIndex);
-
-		// 4. Assert: due to validation, S.mems[a] exists.
-		final Boolean memoryTypeExists = store.getMemoryAll().contains(a);
-		if (memoryTypeExists == false) {
-			throw new WasmRuntimeException(UUID.fromString("034f36d9-c56f-4585-ba5a-a9af1430938b"),
-					"Memory type %s does not exists", a);
-
-		}
-
-		// 5. Let mem be the memory instance S.mems[a].
-		final MemoryType mem = store.getMemoryAll().get(a);
-
-		// 6. Assert: due to validation, a value of value type t is on the top of the stack.
-		if ((stack.peek() instanceof I32) == false) {
-			throw new WasmRuntimeException(UUID.fromString("bbfeab8e-8c59-4c77-929d-03df520a0d5b"),
-					"I32_Store: Step 6: Value type on stack is incorrect.  Expected I32 but type was " + stack.peek().toString());
-		}
-
-		// 7. Pop the value t.const c from the stack
-		I32 c = (I32) stack.pop();
-
-		// 8. Assert: due to validation, a value of value type I32 is on the top of the stack.
-		if ((stack.peek() instanceof I32) == false) {
-			throw new WasmRuntimeException(UUID.fromString("91131cdf-141c-4349-8471-c628b4e64500"),
-					"I32_Store: Step 8: Value type on stack is incorrect.  Expected I32 but type was " + stack.peek().toString());
-		}
-
-		// 9. Pop the value t.const i from the stack
-		I32 i = (I32) stack.pop();
-
-		// 10. Let ea be i+memarg.offset.
-		Long eaValue = i.longValue() + memoryArgument.getOffest().longValue();
-		U32 ea = new U32(eaValue);   // ¿should this be U33?
-
-
-		// 11. If N is NOT part of the instruction, then:
-		//        a: Let N be the bit width |t| of value type t .
-		if (N == null) {
-			N = new U32(32L);
-		}
-
-		// 12. If ea+N/8 is larger than the length of mem.data , then:
-		//        a: Trap.
-		Long length = ea.longValue() + (N.longValue() / 8);
-		if (mem.hasMaximum().integerValue() == 1) {  // not in the webassembly specification.  This may line may be incorrect.
-			Long memLength = mem.maximum().longValue();
-			if (memLength < length) {
-				throw new WasmRuntimeException(UUID.fromString("f51531f2-2f68-462c-a4ab-c8c76c574ffb"),
-						"I32_Store: Step12: Trap.  Address  + size is too large. length = " + length + " memoryLength = " + memLength);
-			}
-		}
-
-		// 13. If N is part of the instruction, then:
-		//    a. Let n be the result of computing wrap|t|,N(c)
-		I32 n = new I32(wrap(N.integerValue(), c.integerValue()));
-
-		//    b. Let b∗ be the byte sequence bytesiN(n).
-		ByteUnsigned[] bytes = n.getBytes();
-
-		// 14.  Else
-		//  a. Let b∗ be the byte sequence bytes t (c).
-		// ByteUnsigned[] bytes = c.getBytes();
-
-		// 15. Replace the bytes mem.data[ea:N/8] with b*.
+	/* package_private */ void step15_ReplaceBytes(MemoryType mem, U32 ea, ByteUnsigned[] bytes) {
 		mem.set(ea.integerValue() + 0, bytes[2]);
 		mem.set(ea.integerValue() + 1, bytes[3]);
-
-	}
-
-	/**
-	 * wrap<sub>M,N</sub>(i)
-	 * <p>
-	 * Return i modulo 2<sup>N</sup>
-	 * <p>
-	 * <b>Source:</b>  <a href="https://webassembly.github.io/spec/core/exec/numerics.html#op-wrap" target="_top">
-	 * https://webassembly.github.io/spec/core/exec/numerics.html#op-wrap
-	 * </a>
-	 *
-	 * @return
-	 */
-	public static Integer wrap(// Integer M,
-	                    Integer N, Integer i) {
-		Integer result;
-		Double pow = Math.pow(2, N);
-		result = (i % pow.intValue());
-		return result;
-
 	}
 
 }
