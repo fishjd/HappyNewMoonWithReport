b3ea79b712a3b5123755984315c4b54d11b585f8
James Haring <fishjd@gmail.com>
2020-05-11 08:53:51 -0600

    Change package name from "Memory" to "memory"

--- src/main/java/happynewmoonwithreport/opcode/Memory/I64_load.java
+++ src/main/java/happynewmoonwithreport/opcode/Memory/I64_load.java
@@ -14,13 +14,12 @@
  *  limitations under the License.
  *
  */
-package happynewmoonwithreport.opcode.Memory;
+package happynewmoonwithreport.opcode.memory;
 
 
 import happynewmoonwithreport.WasmFrame;
 import happynewmoonwithreport.WasmStack;
 import happynewmoonwithreport.WasmStore;
-import happynewmoonwithreport.type.I32;
 import happynewmoonwithreport.type.I64;
 import happynewmoonwithreport.type.IntWasm;
 import happynewmoonwithreport.type.JavaType.ByteUnsigned;
