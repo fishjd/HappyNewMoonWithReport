b095269fa0f4a01addb166fe4c0d870fdb5cfd62
James Haring <fishjd@gmail.com>
2021-06-26 05:41:39 -0600

    Add F64 ceil opcode

---
+++ src/main/java/happynewmoonwithreport/opcode/math/f64/F64_ceil.java
@@ -0,0 +1,115 @@
+/*
+ *  Copyright 2017 - 2021 Whole Bean Software, LTD.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package happynewmoonwithreport.opcode.math.f64;
+
+import happynewmoonwithreport.WasmInstanceInterface;
+import happynewmoonwithreport.WasmRuntimeException;
+import happynewmoonwithreport.WasmStack;
+import happynewmoonwithreport.type.F64;
+import java.util.UUID;
+
+/**
+ * Return the ceiling value of the input.
+ * </p>
+ * <h2>Source:</h2>
+ * <h3>Operator:</h3>
+ * <p>
+ * Return the ceiling value of the input.
+ * <p>
+ * <a href="https://webassembly.github.io/spec/core/exec/numerics.html#op-fceil" target="_top">
+ * https://webassembly.github.io/spec/core/exec/numerics.html#op-fceil
+ * </a>
+ *
+ *
+ * <p>Note: the documentation below is the same for all unary operators.</p>
+ * <h3>Execution:</h3>
+ * <a href="https://webassembly.github.io/spec/core/exec/instructions.html#exec-unop"
+ * target="_top"> https://webassembly.github.io/spec/core/exec/instructions.html#exec-unop
+ * </a>
+ * <h2>t.unop - Unary Operator</h2>
+ *
+ * <ol>
+ * 		<li>
+ * 			Assert: due to validation, a value of value type t is on the top of the stack.
+ * 		</li>
+ * 		<li>
+ * 			Pop the value t.const c1 from the stack.
+ * 		</li>
+ * 		<li>
+ * 			If unopt(c1) is defined, then:
+ * 			<ol type="a">
+ * 				<li>
+ * 					Let c be a possible result of computing unopt(c1).
+ * 				</li>
+ * 				<li>
+ * 					Push the value t.const c to the stack.
+ * 				</li>
+ * 			</ol>
+ * 		</li>
+ * 		<li>
+ * 			Else:
+ * 			<ol type="a">
+ * 				<li>
+ * 					Trap.
+ * 				</li>
+ * 			</ol>
+ * 		</li>
+ * </ol>
+ */
+public class F64_ceil {
+	private final String opCodeName = getClass().getName();
+	private final String t1Type = "F64";
+
+	private WasmInstanceInterface instance;
+
+	private F64_ceil() {
+		super();
+	}
+
+	public F64_ceil(WasmInstanceInterface instance) {
+		this();
+		this.instance = instance;
+	}
+
+	/**
+	 * Execute the opcode.
+	 */
+	public void execute() {
+		WasmStack<Object> stack = instance.stack();
+
+		// Assert: due to validation, a value of value type t is on the top of the stack.
+		if ((stack.peek() instanceof F64) == false) {
+			throw new WasmRuntimeException(UUID.fromString("a49abe1b-075c-471e-9685-b668417c6704"),
+				opCodeName + ":"                                                                 //
+				+ " Value type is incorrect. Value should be of type " + "'" + t1Type + "'" + "."//
+				+ " The input type is " + "'" + stack.peek().getClass().getSimpleName() + "'."   //
+				+ " The input value is " + "'" + stack.peek().toString() + "'.");
+
+		}
+		// Pop the value t1.const c1 from the stack.
+		F64 c1 = (F64) stack.pop();
+
+		//Let c be a possible result of computing unopt(c1).
+		F64 c = c1.ceil();
+
+		// Push the value t.const c to the stack.
+		stack.push(c);
+
+		// No need to trap.
+	}
+}
--- src/test/groovy/happynewmoonwithreport/opcode/math/F32/F32_ceilTest.groovy
+++ src/test/groovy/happynewmoonwithreport/opcode/math/F32/F32_ceilTest.groovy
@@ -82,7 +82,7 @@ class F32_ceilTest extends Specification {
 	 * @param expected The expected value.  What the opcode should return.
 	 * @return None.
 	 */
-	def "Execute F32 ceil #count | #val1_s  || # expected "(Integer count, String val1_s, String expected) {
+	def "Execute F32 ceil #count | #val1_s  || #expected "(Integer count, String val1_s, String expected) {
 		setup: " push one value on stack."
 
 		WasmInstanceInterface instance = new WasmInstanceStub();
